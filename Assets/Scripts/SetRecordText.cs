410e7dcb80c8c65045ee1166ccca19553b80f4a3
joansito <74136459+joansito@users.noreply.github.com>
2023-02-05 15:46:47 +0100

    scorepoints

---
+++ Assets/Scripts/SetRecordText.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetRecordText : MonoBehaviour
+{
+    public Text record;
+    // Start is called before the first frame update
+    void Start()
+    {
+        record.text = PlayerPrefs.GetInt("record").ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        record.text = PlayerPrefs.GetInt("record").ToString();
+    }
+}
